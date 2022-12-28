a131ed895686aa66f051cad75bcc1be3aac8e5fb
ErickMesquita <erick.mesquita@usp.br>
2022-12-28 17:58:15 -0300

    QuickSort 3 way

--- quicksort.go
+++ quicksort.go
@@ -13,6 +13,19 @@ func QuickSort[T any](array []T, less func(T, T) bool) {
 	}
 }
 
+func QuickSort3way[T any](array []T, less func(T, T) bool) {
+	if len(array) <= 1 {
+		return
+	}
+
+	var middle_start, middle_end int = positionPivot3way(array, less)
+	QuickSort(array[:middle_start], less)
+
+	if middle_end+1 < len(array) {
+		QuickSort(array[middle_end+1:], less)
+	}
+}
+
 func positionPivotLomuto[T any](array []T, less func(T, T) bool) int {
 	if len(array) <= 1 {
 		return 0
@@ -73,4 +86,48 @@ func positionPivotHoare[T any](array []T, less func(T, T) bool) int {
 	array[length-1], array[low] = array[low], array[length-1]
 
 	return low
+}
+
+func positionPivot3way[T any](array []T, less func(T, T) bool) (int, int) {
+	if len(array) <= 1 {
+		return 0, 0
+	}
+
+	end := len(array) - 1
+
+	var greaterOrEqualIndex int = 0
+
+	for i := 0; i < end; i++ {
+		if !less(array[i], array[end]) {
+			continue
+		}
+
+		if i != greaterOrEqualIndex {
+			array[i], array[greaterOrEqualIndex] = array[greaterOrEqualIndex], array[i]
+		}
+
+		greaterOrEqualIndex++
+	}
+
+	var equalElemsIndex int = end - 1
+
+	for i := end - 1; i >= greaterOrEqualIndex; i-- {
+		// pivot >= array[i]
+		if !less(array[end], array[i]) {
+			continue
+		}
+
+		if i != equalElemsIndex {
+			array[i], array[equalElemsIndex] = array[equalElemsIndex], array[i]
+		}
+		equalElemsIndex--
+	}
+
+	equalElemsIndex++
+
+	if end != equalElemsIndex {
+		array[end], array[equalElemsIndex] = array[equalElemsIndex], array[end]
+	}
+
+	return greaterOrEqualIndex, equalElemsIndex
 }
--- quicksort_test.go
+++ quicksort_test.go
@@ -11,66 +11,76 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+type Test struct {
+	name  string
+	array []int
+}
+
+var tests []Test = []Test{
+	{
+		name:  "Test small sorted array",
+		array: []int{0, 0, 0, 0, 1, 1, 1, 2, 2, 2},
+	},
+	{
+		name:  "Test small reversed array",
+		array: []int{2, 2, 2, 1, 1, 1, 0, 0, 0},
+	},
+	{
+		name:  "Test pivot between",
+		array: []int{0, 0, 0, 0, 1, 1, 1, 2, 2, 2, 2, 1},
+	},
+	{
+		name:  "Test small array",
+		array: []int{10, 8, 6, 4, 2, 0},
+	},
+	{
+		name: "Test longer array",
+		array: []int{20, 19, 18, 17, 17, 17, 16, 16, 15, 14,
+			13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0},
+	},
+	{
+		name: "Test enormous array",
+		array: []int{
+			10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 456, 789, 132, 4, 321, 12, 0, 5, 12,
+			10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 456, 789, 132, 4, 321, 12, 0, 5, 12,
+			10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 456, 789, 132, 4, 321, 12, 0, 5, 12,
+			10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 456, 789, 132, 4, 321, 12, 0, 5, 12,
+			10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 456, 789, 132, 4, 321, 12, 0, 5, 12,
+			10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 456, 789, 132, 4, 321, 12, 0, 5, 12,
+			10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 456, 789, 132, 4, 321, 12, 0, 5, 12,
+		},
+	},
+}
+
 func TestQuickSort(t *testing.T) {
-	lessInt := func(a, b int) bool {
-		return a < b
-	}
-
-	type args struct {
-		array []int
-	}
-
-	tests := []struct {
-		name string
-		args args
-	}{
-		{
-			name: "Test small array",
-			args: args{
-				array: []int{10, 8, 6, 4, 2, 0},
-			},
-		},
-		{
-			name: "Test bigger array",
-			args: args{
-				array: []int{10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0},
-			},
-		},
-		{
-			name: "Test enormous array",
-			args: args{
-				array: []int{
-					10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 456, 789, 132, 4, 321, 12, 0, 5, 12,
-					10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 456, 789, 132, 4, 321, 12, 0, 5, 12,
-					10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 456, 789, 132, 4, 321, 12, 0, 5, 12,
-					10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 456, 789, 132, 4, 321, 12, 0, 5, 12,
-					10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 456, 789, 132, 4, 321, 12, 0, 5, 12,
-					10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 456, 789, 132, 4, 321, 12, 0, 5, 12,
-					10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 456, 789, 132, 4, 321, 12, 0, 5, 12,
-				},
-			},
-		},
-	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			QuickSort(tt.args.array, lessInt)
+			QuickSort(tt.array, lessInt)
 
-			assert.IsNonDecreasing(t, tt.args.array, "Array should be in ascending order")
+			assert.IsNonDecreasing(t, tt.array, "Array should be in ascending order")
+		})
+	}
+}
+
+func TestQuickSort3way(t *testing.T) {
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			QuickSort3way(tt.array, lessInt)
+			fmt.Println(tt.array)
+
+			assert.IsNonDecreasing(t, tt.array, "Array should be in ascending order")
 		})
 	}
 }
 
 func FuzzQuickSort(f *testing.F) {
-	lessInt := func(a, b int) bool {
-		return a < b
-	}
-
 	f.Add(uint(22122022)) // Use f.Add to provide a seed corpus
 
 	f.Fuzz(func(t *testing.T, ArraySize uint) {
 		var array = make([]int, ArraySize)
-		log.Println("ArraySize:", ArraySize)
+		//log.Println("ArraySize:", ArraySize)
 
 		for i := uint(0); i < ArraySize; i++ {
 			array[i] = int(rand.Int63() - math.MaxInt64/2)
@@ -121,8 +131,12 @@ func BenchmarkQuickSortFromFile(b *testing.B) {
 	for _, v := range testSizes {
 		b.Run(fmt.Sprintf("input size: %d", v.nrows), func(b *testing.B) {
 			for i := 0; i < b.N; i++ {
-				QuickSort(array[:v.nrows], func(a, b int) bool { return a < b })
+				QuickSort(array[:v.nrows], lessInt)
 			}
 		})
 	}
+}
+
+func lessInt(a, b int) bool {
+	return a < b
 }
