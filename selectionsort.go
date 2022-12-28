ad3923965114151d06acce941cfab34ef237df84
ErickMesquita <erick.mesquita@usp.br>
2022-12-28 17:59:06 -0300

    Bug fixed on SelectionSort

--- selectionsort.go
+++ selectionsort.go
@@ -2,7 +2,8 @@ package sort
 
 func SelectionSort[T any](array []T, less func(T, T) bool) {
 	for i := 0; i < len(array); i++ {
-		_, minIndex := min(array, less)
+		_, minIndex := min(array[i:], less)
+		minIndex += i
 		array[i], array[minIndex] = array[minIndex], array[i]
 	}
 }
